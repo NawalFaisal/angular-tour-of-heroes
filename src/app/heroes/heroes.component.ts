97eb0482a4afb81c0c57147bca2518a9f8f75229
NawalFaisal <nawal.fais@outlook.com>
2026-05-07 14:59:27 -0600

    hero service with observable pattern and dependency injection

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,34 @@
 import { Component } from '@angular/core';
 import{Hero} from '../hero';
 import {UpperCasePipe, NgFor } from '@angular/common';
-import { HEROES } from '../mock.heroes';
+import { HeroService } from '../hero.service';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+
 
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, NgFor,HeroDetailComponent],
+  imports: [UpperCasePipe, NgFor,HeroDetailComponent,],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 
 export class HeroesComponent {
-  heroes = HEROES;
-
-
-  // this is property so it has : 
+  heroes: Hero[] = [];
   selectedHero?: Hero;
-  //this is method so it has () taking params. and retruns type is empty
+
+  constructor(private heroService: HeroService){}
+
+  ngOnInit(): void{
+    this.getHeroes();
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
   onSelect(hero:Hero): void{
     this.selectedHero = hero;
   }
